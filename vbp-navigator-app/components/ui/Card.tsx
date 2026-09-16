fe98232952858aa9092886ccb6148a88a91221e1
Diallo Mavube <biz.process@proton.me>
2026-09-16 16:29:36 +0300

    Clean up Done
    Removed duplicate dir

--- vbp-navigator-app/components/ui/Card.tsx
+++
@@ -1,5 +0,0 @@
-import { HTMLAttributes } from "react";
-
-export function Card({ className = "", ...rest }: HTMLAttributes<HTMLDivElement>) {
-  return <div className={`v2-card ${className}`} {...rest} />;
-}
